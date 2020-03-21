c813ea0c08598b77dcf5d72b397a2827a17883c5
dustinpfister <dustin.pfister@gmail.com>
2020-03-21 11:34:40 -0400

    demo variable

---
+++ demo_variable.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+let genMap = require('./lib/genmaps.js');
+
+// variable map section size
+let world = genMap.genWorldMap({
+        width: 2,
+        height: 2,
+        forMapSecOptions: function (msOpt) {
+            let secWidth = 1 + msOpt.secIndex;
+            msOpt.secWidth = secWidth;
+            msOpt.secHeight = 2;
+            msOpt.data = {
+                secWidth: secWidth
+            };
+            return msOpt;
+        }
+    });
+
+console.log(JSON.stringify(world));
