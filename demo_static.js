5fa1dd5ca8956c3c3ac39bad01d86780946a8af2
dustinpfister <dustin.pfister@gmail.com>
2020-03-21 11:58:58 -0400

    passing world object

--- demo_static.js
+++ demo_static.js
@@ -8,18 +8,20 @@ let world = genMap.genWorldMap({
         height: 3,
         secWidth: 2,
         secHeight: 2,
-        forMapSecOptions: function (msOpt) {
+        forMapSecOptions: function (msOpt, world) {
             let i = msOpt.secIndex,
+            cx = Math.floor(world.width / 2),
+            cy = Math.floor(world.height / 2),
             secX = i % 2,
             secY = Math.floor(i / 2),
-            d = Math.sqrt(Math.pow(secX - 1, 2) + Math.pow(secY - 1, 2));
+            d = Math.sqrt(Math.pow(secX - cx, 2) + Math.pow(secY - cy, 2));
             msOpt.data = {
                 secX: secX,
                 secY: secY,
                 val: d * 10 + 5
             };
             msOpt.forCell = function (cell) {
-                cell.val = msOpt.data.val / (2 * 2);
+                cell.val = msOpt.data.val / (world.secWidth * world.secHeight);
                 return cell;
             };
             return msOpt;
