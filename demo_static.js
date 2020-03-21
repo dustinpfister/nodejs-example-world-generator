77d90ffa3f5cbb3e0196041122ff4cc9335db438
dustinpfister <dustin.pfister@gmail.com>
2020-03-21 11:42:33 -0400

    demo_static

---
+++ demo_static.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+let genMap = require('./lib/genmaps.js');
+
+// variable map section size
+let world = genMap.genWorldMap({
+        width: 3,
+        height: 3,
+        secWidth: 2,
+        secHeight: 2,
+        forMapSecOptions: function (msOpt) {
+            let i = msOpt.secIndex,
+            secX = i % 2,
+            secY = Math.floor(i / 2),
+            d = Math.sqrt(Math.pow(secX - 1, 2) + Math.pow(secY - 1, 2));
+            msOpt.data = {
+                secX: secX,
+                secY: secY,
+                val: d * 10 + 5
+            };
+            return msOpt;
+        }
+    });
+
+console.log(JSON.stringify(world));
